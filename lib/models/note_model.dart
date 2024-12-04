2d7bbd6d0aa990d5774d4eaacbf5b17dc3215b6b
zakaryajawish <zakaryajawish@gmail>
2024-12-05 00:04:43 +0300

    create note model - finish note view

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class NoteModel {
+  String title;
+  String body;
+  String date;
+  int color;
+  NoteModel({
+    required this.title,
+    required this.body,
+    required this.date,
+    required this.color,
+  });
+}
--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,3 +1,4 @@
+import 'package:dawwen_app/extensions/context_ex.dart';
 import 'package:dawwen_app/views/edit_note_view.dart';
 import 'package:dawwen_app/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
@@ -15,7 +16,29 @@ class NoteView extends StatelessWidget {
           onPressed: () {
             Navigator.pushNamed(context, EditNoteView.id);
           }),
-      body: Expanded(child: Text('data')),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Expanded(
+            child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'data datat data body here body here bodey data body here body here bodey ',
+                style: context.textTheme.bodyMedium!
+                    .copyWith(fontSize: 26, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                'here rhrer  body here body here bodey here rhrer  body here body here bodey here rhrer  body here body here bodey here rhrer  body here body here bodey here rhrer hherhererherherhhhhhhhhhhhdata body here body here bodey here rhrer hherhererherherhhhhhhhhhhh hherhererherherhhhhhhhhhhhhhhhhhhhh ',
+                style: context.textTheme.bodyMedium!.copyWith(fontSize: 23),
+              ),
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
