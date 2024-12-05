e1609716444c09f6849823664ec888a2fa6dde06
zakaryajawish <zakaryajawish@gmail>
2024-12-05 19:49:17 +0300

    create type adabter for note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,15 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   String title;
+  @HiveField(1)
   String body;
+  @HiveField(2)
   String date;
+  @HiveField(3)
   int color;
   NoteModel({
     required this.title,
