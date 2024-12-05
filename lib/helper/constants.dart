79b51aae2209d5bd581cf194794ddba0e2df8c3c
zakaryajawish <zakaryajawish@gmail>
2024-12-05 22:01:16 +0300

    add hive init flutter - open box

--- lib/helper/constants.dart
+++ lib/helper/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 Color kPrimaryColor = const Color.fromARGB(255, 77, 135, 173);
 Color kPrimaryBlack = Colors.grey.shade900;
+const String kNoteBox = "NoteBox";
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:dawwen_app/helper/constants.dart';
 import 'package:dawwen_app/themes/themes.dart';
 import 'package:dawwen_app/views/home_view.dart';
 import 'package:dawwen_app/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'views/edit_note_view.dart';
 import 'views/note_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNoteBox);
+
   runApp(const DawwenApp());
 }
 
