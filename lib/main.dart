a21a8836965d2e6ab841f893e9efdbc087ed26bc
zakaryajawish <zakaryajawish@gmail>
2025-01-20 17:57:20 +0300

    add blucOpserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:dawwen_app/bloc_observer.dart';
 import 'package:dawwen_app/helper/constants.dart';
 import 'package:dawwen_app/models/note_model.dart';
 import 'package:dawwen_app/themes/themes.dart';
 import 'package:dawwen_app/views/home_view.dart';
 import 'package:dawwen_app/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'views/edit_note_view.dart';
@@ -11,6 +13,7 @@ import 'views/note_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = SimpleBlocOpserver();
   await Hive.initFlutter();
   await Hive.openBox(kNoteBox);
   Hive.registerAdapter(NoteModelAdapter());
