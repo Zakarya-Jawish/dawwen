c091d67412a74b7c5b02e0d15c031e5b7a0fea6a
zakaryajawish <zakaryajawish@gmail>
2025-01-22 13:49:58 +0300

    create getNotes method in notes cubit

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:dawwen_app/helper/constants.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+
+import '../../models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitialState());
+  List<NoteModel> notes = [];
+  getAllNotes() {
+    //we don't need loading state because nothing need wait
+    try {
+      var noteBox = Hive.box<NoteModel>(kNoteBox);
+      notes = noteBox.values.toList();
+      emit(NotesSuccessState());
+    } catch (e) {
+      emit(NotesFailureState(e.toString()));
+    }
+  }
 }
