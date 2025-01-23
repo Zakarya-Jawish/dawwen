c182aedfd0fb8a06c27c252d8bc0158fdf71ec66
zakaryajawish <zakaryajawish@gmail>
2025-01-23 14:26:25 +0300

    provide NotesCubit - create blocBuilder for note view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../cubits/notes_cubit/notes_cubit.dart';
 import '../widgets/add_note_bottom_sheet.dart';
 import '../widgets/custom_appbar.dart';
 import '../widgets/drawer.dart';
@@ -10,26 +12,29 @@ class HomeView extends StatelessWidget {
   static String id = 'homeview';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const CustomDrawer(),
-      appBar: CustomAppBar(
-        title: 'Notes',
-        iconData: Icons.search,
-        onPressed: () {},
-      ),
-      body: const Padding(
-        padding: EdgeInsets.all(10),
-        child: NoteListBuilder(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (context) => const AddNoteBottomSheet(),
-          );
-        },
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        drawer: const CustomDrawer(),
+        appBar: CustomAppBar(
+          title: 'Notes',
+          iconData: Icons.search,
+          onPressed: () {},
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(10),
+          child: NoteListBuilder(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) => const AddNoteBottomSheet(),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/widgets/list_builder_note.dart
+++ lib/widgets/list_builder_note.dart
@@ -1,14 +1,20 @@
+import 'package:dawwen_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:dawwen_app/widgets/note_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NoteListBuilder extends StatelessWidget {
   const NoteListBuilder({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) => const NoteCard(),
-      itemCount: 3,
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        return ListView.builder(
+          itemBuilder: (context, index) => const NoteCard(),
+          itemCount: 3,
+        );
+      },
     );
   }
 }
